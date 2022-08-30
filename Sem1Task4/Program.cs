76f02ba407b739fae4eb7691153ff70f99cb7473
Kirill Grigorev <grigorevkirill@mail.ru>
2022-08-30 23:43:48 +0300

    added Sem1Task4

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿//=========================================================
+// # 3 Напишите программу, которая принимать два числа и
+// выдавать наибольшее из этих чисел.
+//=========================================================
+
+string? inputlineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputlineA != null && inputLineB != null)
+{
+    int firstNumber = int.Parse(inputlineA);
+    int secondNumber = int.Parse(inputLineB);
+
+    if (firstNumber > secondNumber)
+    {
+        Console.Write(firstNumber + " > " + secondNumber);
+    }
+    else
+    {
+        Console.Write(secondNumber + " > " + firstNumber);
+    }
+}
