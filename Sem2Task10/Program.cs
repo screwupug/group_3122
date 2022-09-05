827f228dbad9b67238e18a65cd055990dfdba451
Kirill Grigorev <grigorevkirill@mail.ru>
2022-09-05 19:53:28 +0300

    fixed Sem2Task10 name

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,43 @@
+﻿//=============================================================================
+// # 10 Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// * показывает второю цифру любого числа
+//=============================================================================
+
+// // Решение 1
+// Console.Write("Введите трехзначное число: ");
+// string? inputLine = Console.ReadLine();
+
+// if (inputLine != null && inputLine.Length == 3)
+// {
+//     int inputNumber = int.Parse(inputLine);
+//     char[] charArray = inputNumber.ToString().ToCharArray();
+//     Console.WriteLine($"Вторая цифра числа {inputNumber} - {charArray[1]}");
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели неправильное число");
+// }
+
+// Решение 2 со *
+
+Console.Write("Введите любое число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    if(inputLine.Length >= 2)
+    {
+        int inputNumber = int.Parse(inputLine);
+        char[] charArray = inputNumber.ToString().ToCharArray();
+        Console.WriteLine($"Вторая цифра числа {inputNumber} - {charArray[1]}");
+    }
+    else
+    {
+        Console.WriteLine("У введенного числа нет второй цифры");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели неправильное число");
+}
