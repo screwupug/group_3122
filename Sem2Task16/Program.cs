146b73208eaa160b1c86eb6cbc7e892fa20b1ab1
Kirill Grigorev <grigorevkirill@mail.ru>
2022-09-06 21:49:47 +0300

    added Sem2Task16 and Sem2Task18

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,46 @@
+﻿//=============================================================================
+// # 16 Напишите программу, которая будет принимать на вход два числа и проверяет
+// является ли одно число квадратом другого.
+//=============================================================================
+
+
+int ReadData(string line)
+{   
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine()??"0");
+    // Возвращаем значение
+    return number;
+}
+
+// Тест на квадрат
+bool SqrTest(int firstNumber, int secondNumber)
+{
+    if(firstNumber == secondNumber * secondNumber)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void PrintData(int firstNumber, int secondNumber)
+{
+    if(SqrTest(firstNumber, secondNumber))
+    {
+        Console.WriteLine($"Число {firstNumber} является квадратом числа {secondNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {firstNumber} не является квадратом числа {secondNumber}");
+    }
+}
+
+int num1 = ReadData("Ведите первое число: ");
+int num2 = ReadData("Ведите второе число: ");
+
+PrintData(num1, num2);
+PrintData(num2, num1);
---
+++ Sem2Task18/Program.cs
@@ -0,0 +1,39 @@
+﻿//=============================================================================
+// # 18 Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных
+// координат точек в этой четверти (x и y).
+//=============================================================================
+
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+string QuarterBorderAsk(int numQuarter)
+{
+    if (numQuarter == 1)
+        return "x > 0, x > 0";
+    if (numQuarter == 2)
+        return "x < 0, x > 0";
+    if (numQuarter == 3)
+        return "x < 0, x < 0";
+    if (numQuarter == 4)
+        return "x > 0, x < 0";
+
+    return "";
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите номер четверти: ");
+string res = QuarterBorderAsk(num);
+
+PrintResult(res);
