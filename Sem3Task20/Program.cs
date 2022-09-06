ea153420edbcdc1edbd173c4a40a6eef7a488552
Kirill Grigorev <grigorevkirill@mail.ru>
2022-09-06 21:48:56 +0300

    added new tasks from Sem3

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,35 @@
+﻿//=============================================================================
+// # 20 Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// Расстояное - сумма квадратов катетов AB2 = (y1 - y2)2 + (x1 - x2)2
+//=============================================================================
+
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+double CalculateData(int x1, int x2, int y1, int y2)
+{
+    double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return res;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int x1 = ReadData("Введите x1: ");
+int x2 = ReadData("Введите x2: ");
+int y1 = ReadData("Введите y1: ");
+int y2 = ReadData("Введите y2: ");
+double res = CalculateData(x1, y1, x2, y2);
+
+PrintResult($"{Math.Round(res, 2)}");
---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,54 @@
+﻿//=============================================================================
+// # 22 Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+//=============================================================================
+
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+string LineNubers(int numberN)
+{
+    int i = 0;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        outLine = outLine + i + "\t";
+        i++;
+    }
+    outLine = outLine + numberN;
+    return outLine;
+}
+
+string SquareLine(int numberN)
+{
+    int i = 0;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        outLine = outLine + i * i + "\t";
+        i++;
+    }
+    outLine = outLine + i * i;
+    return outLine;
+
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите исло N: ");
+string lineTop = LineNubers(num);
+string lineDown = SquareLine(num);
+
+PrintResult(lineTop);
+PrintResult(lineDown);
+
